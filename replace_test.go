48439b681345bd4bdf203dc18cda569f1145cf3d
leep-frog <gleeper49@gmail.com>
2021-08-29 14:00:48 -0400

    update command package

--- replace_test.go
+++ replace_test.go
@@ -241,7 +241,7 @@ func TestRecursive(t *testing.T) {
 				baseDirectory: dir,
 			}
 			test.etc.Node = r.Node()
-			command.ExecuteTest(t, test.etc, nil)
+			command.ExecuteTest(t, test.etc)
 			command.ChangeTest(t, nil, r)
 
 			for f, originalContents := range test.files {
